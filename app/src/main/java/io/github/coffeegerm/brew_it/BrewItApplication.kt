b14c825419db4563015626436c4df0a5201a19e4
David Yarzebinski <dyarzebinski97@gmail.com>
2017-11-05 11:23:03 -0500

    realm init

--- app/src/main/java/io/github/coffeegerm/brew_it/BrewItApplication.kt
+++ app/src/main/java/io/github/coffeegerm/brew_it/BrewItApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import io.github.coffeegerm.brew_it.dagger.AppComponent
 import io.github.coffeegerm.brew_it.dagger.AppModule
 import io.github.coffeegerm.brew_it.dagger.DaggerAppComponent
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 /**
  * Created by dYarzebinski on 10/23/2017.
@@ -25,8 +27,18 @@ class BrewItApplication : Application() {
                 .build()
     }
 
+    private fun initRealm() {
+        Realm.init(this)
+        val realmConfig: RealmConfiguration = RealmConfiguration
+                .Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        Realm.setDefaultConfiguration(realmConfig)
+    }
+
     override fun onCreate() {
         super.onCreate()
+        initRealm()
         appComponent = initDagger(this)
     }
 }
