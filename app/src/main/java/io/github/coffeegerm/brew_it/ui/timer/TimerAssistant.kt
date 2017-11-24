7827a293317aebebd08d611b9ba5b1b3a60fe724
David Yarzebinski <dyarzebinski97@gmail.com>
2017-11-24 12:32:58 -0500

    update

---
+++ app/src/main/java/io/github/coffeegerm/brew_it/ui/timer/TimerAssistant.kt
@@ -0,0 +1,15 @@
+package io.github.coffeegerm.brew_it.ui.timer
+
+/**
+ * Created by david_yarz on 11/23/17.
+ */
+
+class TimerAssistant() {
+
+    private var drinkMade: Int = 0
+
+    fun setDrink(drinkResId: Int) {
+        this.drinkMade = drinkResId
+    }
+
+}
